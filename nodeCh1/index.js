e549e4af017fa5380203e3c77fb2795391ef0df5
crystaldonga <crystaldonga79@gmail.com>
2024-01-14 11:41:10 +0530

    nodecchallange-1

---
+++ nodeCh1/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+//fs.mkdirSync('thapa');
+//fs.writeFileSync("thapa/bio.txt",'my name is crystal  ');
+//fs.appendFileSync('thapa/bio.txt','IT');
+//const data = fs.readFileSync('thapa/bio.txt','utf8');
+//console.log(data);
+//fs.renameSync('thapa/bio.txt','thapa/mybio.txt');
+//fs.unlinkSync('thapa/mybio.txt');
+//fs.rmdirSync('thapa');
+
