dbe5e602af80a8c9adbb4c6459ee5315b7b078b5
crystaldonga <crystaldonga79@gmail.com>
2024-01-14 11:52:54 +0530

    7

---
+++ ModeWrapperFunction/index.js
@@ -0,0 +1,9 @@
+// (function(exports,require,module,__filename,__dirname) {
+//    // const a = require('fs')
+//     const name ="vinod";
+//     console.log(name);
+    
+//     //module.exports={dfsgsbw}
+// })();//fe 
+console.log(__dirname);
+console.log(__filename);
