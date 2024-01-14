363d9206316d9095a073a087c24515e2e35ffb89
crystaldonga <crystaldonga79@gmail.com>
2024-01-14 11:57:50 +0530

    10

---
+++ ownModule/index.js
@@ -0,0 +1,8 @@
+ const {add,sub,mul,name} = require('./oper');
+ const oper= require('./oper');
+ 
+console.log(add(5,5));
+console.log(sub(5,5));
+console.log(mul(5,5));
+console.log(name);
+console.log(oper);
