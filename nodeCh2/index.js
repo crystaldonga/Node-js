446bed10b775ae502f3191473e25c29a2eeaec54
crystaldonga <crystaldonga79@gmail.com>
2024-01-14 11:42:51 +0530

    node challange-2

---
+++ nodeCh2/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+// fs.mkdir('thapa',(err)=>{
+//     console.log("new file is created");
+// })
+
+// fs.writeFile('./thapa/bio.txt',"hello good morning",(err)=>{
+//     console.log('txt file is created');
+// })
+
+// fs.appendFile("./thapa/bio.txt",'  ye kya chal raha he',(err)=>{
+//     console.log("sucessfully append");
+// })
+
+// fs.readFile('./thapa/bio.txt','utf-8',(err,data)=>{
+//     console.log(err);
+//     console.log(data);
+// })
+
+// fs.rename('./thapa/bio.txt','./thapa/mybio.txt',(err)=>{
+//     console.log("rename sucessfully");
+// })
+
+// fs.unlink('./thapa/mybio.txt',(err)=>{
+//     console.log("deleted");
+// })
+// fs.rmdir('./thapa',(err)=>{
+//     console.log("remove folder");
+// })
